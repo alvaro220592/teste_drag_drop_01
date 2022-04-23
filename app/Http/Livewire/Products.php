f7694b939afdaabb979cdeb2cd1ef0b78b73b9d5
alvaro220592 <alvaro220592@gmail.com>
2022-04-23 18:50:05 -0300

    ...

---
+++ app/Http/Livewire/Products.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class Products extends Component
+{
+    public function render()
+    {
+        $products = Product::all();
+        return view('livewire.products', [
+            'products' => $products
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Livewire\Products;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/products', Products::class);
